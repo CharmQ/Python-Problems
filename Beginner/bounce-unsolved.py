3916969e6f2a281afd624084abd247f583a72411
Cliff <cliffvong123@gmail.com>
2022-05-28 18:02:43 -0400

    added more exercises

---
+++ Beginner/bounce-unsolved.py
@@ -0,0 +1,9 @@
+'''
+Bounce Problem
+
+A rubber ball is dropped from a height of 100 meters and each time it
+hits the ground, it bounces back up to 3/5 the height it fell. Write
+a program that prints a table showing the height of the first 10 bounces.
+
+As an additional challenge, round all answers to 4 decimal places using "%0.2f" % (num,)
+'''
---
+++ Beginner/namecreation-unsolved.py
@@ -0,0 +1,29 @@
+'''
+
+Creat your character (name) Problem
+
+Create a program that asks a user for a first name and last name (separately), the program should then display some sort of welcome message to that user's full name. This is an exercise of handling basic string manipulation.
+
+for example:
+
+bash
+% What is your first name? 
+% >>> John
+% What is your last name?
+% >>> Doe
+
+% Welcome to hell, John Doe, we hope you enjoy your stay!
+
+Furthermore, the program should be able to handle unclean inputs that have whitespaces or wrong capitalizations:
+
+for example:
+
+bash
+% What is your first name?
+% >>> [tab space]jOhN [space]
+What is your last name?
+% >>> [space] dOE [space]
+
+% Welcome to hell, John Doe, we hope you enjoy your stay!
+
+'''
