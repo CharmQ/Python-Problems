863fa9c708748316fc52f41bbefa694e72f00c58
Cliff <cliffvong123@gmail.com>
2022-06-01 00:44:14 -0400

    added more problems

--- beginner/unsolved/towerofhanoi-unsolved.py
+++ beginner/unsolved/towerofhanoi-unsolved.py
@@ -1,4 +1,6 @@
 '''
+Tower of Hanoi Problem
+
 In the classic problem of the Towers of Hanoi, you have 3 towers and N disks of different sizes which can slide onto any tower. The puzzle starts with disks sorted in ascending order of size from top to bottom (i.e., each disk sits on top of an even larger one).
 
 You have the following constraints:
